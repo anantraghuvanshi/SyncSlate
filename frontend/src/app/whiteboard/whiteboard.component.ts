b6cdf395d50e82368cd4348ec33fbd11312cc5c0
Anant Singh Raghuvanshi <anantsinghraghuvanshi1@gmail.com>
2023-09-29 01:42:49 +0530

    whiteboard basic functionality done

--- frontend/src/app/whiteboard/whiteboard.component.ts
+++ frontend/src/app/whiteboard/whiteboard.component.ts
@@ -12,29 +12,28 @@ export class WhiteboardComponent {
   canvas!: HTMLCanvasElement;
   ctx!: CanvasRenderingContext2D;
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
     this.canvas = this.canvasRef.nativeElement;
-    this.ctx = this.canvas.getContext('2d')!;
-    this.canvas.width = 800;
-    this.canvas.height = 600;
+    this.ctx = this.canvas.getContext('2d') as CanvasRenderingContext2D;
+    this.ctx.lineWidth = 2;
+    this.ctx.lineCap = 'round';
+    this.ctx.strokeStyle = '#000';
+
+    this.canvas.width = window.innerWidth * 0.8;
+    this.canvas.height = window.innerHeight * 0.8;
   }
 
   startDrawing(event: MouseEvent): void {
+    const rect = this.canvas.getBoundingClientRect();
     this.isDrawing = true;
     this.ctx.beginPath();
-    this.ctx.moveTo(
-      event.clientX - this.canvas.offsetLeft,
-      event.clientY - this.canvas.offsetTop
-    );
+    this.ctx.moveTo(event.clientX - rect.left, event.clientY - rect.top);
   }
 
   doDrawing(event: MouseEvent): void {
     if (!this.isDrawing) return;
-
-    this.ctx.lineTo(
-      event.clientX - this.canvas.offsetLeft,
-      event.clientY - this.canvas.offsetTop
-    );
+    const rect = this.canvas.getBoundingClientRect();
+    this.ctx.lineTo(event.clientX - rect.left, event.clientY - rect.top);
     this.ctx.stroke();
   }
 
